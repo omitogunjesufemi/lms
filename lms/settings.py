ebc295561e992d20c892be70565d23fc8fdb6efa
omitogunjesufemi <omitogunopeyemi@gmail.com>
2021-01-20 18:47:17 +0100

    added

--- lms/settings.py
+++ lms/settings.py
@@ -24,7 +24,7 @@ dotenv_path = join(dirname(__file__), '.env')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_LEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
@@ -135,4 +135,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'lms_app/static'),
 )
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
