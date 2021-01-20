13f628798679da9f819152e45306e8fe84bd9b95
Jesufemi Omitogun <72797681+omitogunjesufemi@users.noreply.github.com>
2021-01-20 18:53:30 +0100

    Update settings.py
    changed static path

--- lms/settings.py
+++ lms/settings.py
@@ -127,7 +127,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'Files')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'lms_app/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
