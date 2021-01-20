12cebe56ca48809f372db5ae2918794ce88f2918
Jesufemi Omitogun <72797681+omitogunjesufemi@users.noreply.github.com>
2021-01-20 18:43:52 +0100

    Update settings.py
    CHANGES

--- lms/settings.py
+++ lms/settings.py
@@ -24,7 +24,7 @@ dotenv_path = join(dirname(__file__), '.env')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_LEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
