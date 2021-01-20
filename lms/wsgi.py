da6dedc93dda77f48be596ccbb84e0fa63a271d2
omitogunjesufemi <omitogunopeyemi@gmail.com>
2021-01-20 19:01:07 +0100

    added

--- lms/wsgi.py
+++ lms/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms.settings')
 
+# from whitenoise.django import DjangoWhiteNoise
+# application = DjangoWhiteNoise(application)
+
 application = get_wsgi_application()
